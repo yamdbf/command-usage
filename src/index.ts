ff44907f56c5a9462caf593afb80c69b19f9138d
Zack Campbell <zajrik@gmail.com>
2017-07-21 22:41:00 -0500

    Fix argument name typo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { PluginConstructor } from 'yamdbf';
 import { CommandUsage } from './CommandUsage';
 
-const commandUsage: (guild: string) => PluginConstructor = CommandUsage.commandUsage;
+const commandUsage: (channel: string) => PluginConstructor = CommandUsage.commandUsage;
 export { commandUsage };
 export { CommandUsage };
 export default commandUsage;
